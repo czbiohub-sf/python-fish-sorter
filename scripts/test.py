b1e2ce20bc4d1bc45870f9fac153f43de58fe6fc
unknown <michelle.khoo@czbiohub.org>
2024-02-15 19:09:00 -0800

    mmc+ working

--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,15 @@
+import pymmcore
+
 from pymmcore_plus import CMMCorePlus
+from pathlib import Path
+
+parent_dir = Path("C:/Program Files/Micro-Manager-2.0-20240130")
+config_file = "test.cfg"
+config_dir = parent_dir / config_file
 
 mmc = CMMCorePlus()
-mmc.loadSystemConfiguration()
+mmc.setDeviceAdapterSearchPaths([str(parent_dir)])
+mmc.loadSystemConfiguration("test.cfg")
+
+mmc.snapImage()
+print(mmc.getImage())
