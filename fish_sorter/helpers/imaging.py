d90653c465625d2c1c28ce9d410a8616a9003440
Michelle Khoo <michelle.khoo.wl@gmail.com>
2024-08-14 16:04:44 -0700

    Gets metadata out

--- fish_sorter/helpers/imaging.py
+++ fish_sorter/helpers/imaging.py
@@ -8,7 +8,11 @@ from typing import cast
 
 from pathlib import Path
 from useq import MDASequence, Position
+from useq._iter_sequence import _sizes, _used_axes, _iter_axis, _parse_axes
+
 from gui.pipette_gui import PipetteWidget
+
+from itertools import product
 
 try:
     from pymmcore_widgets.useq_widgets import PYMMCW_METADATA_KEY as PYMMCW_METADATA_KEY
@@ -33,17 +37,18 @@ class MosaicHandler:
                 {"config": "GFP","exposure": 100}, 
                 {"config": "TXR", "exposure": 100}
             ],
-            stage_positions = [
-                # {"x": 110495.44, "y": 10863.76, "z": 2779.09, "name": "top_R"},
-                # {"x": 17883.77, "y" : 10166.54, "z": 2779.09, "name": "top_L"},
-                # {"x": 110495.44, "y": 73208.59, "z": 2776.70, "name": "bot_R"},
-                # {"x": 17492.82, "y": 73208.58, "z": 2776.70, "name": "bot_L"},
-                Position(
-                    x=17883.77, y=10166.54, z=2779.09, name= "array", 
-                    sequence=MDASequence(
-                        grid_plan={"rows": 3, "columns": 4, "relative_to": "top_left", "overlap": 5, "mode": "row_wise_snake"})
-                ),
-            ],
+            grid_plan={"rows": 3, "columns": 4, "relative_to": "top_left", "overlap": 5, "mode": "row_wise_snake"},
+            # stage_positions = [
+            #     # {"x": 110495.44, "y": 10863.76, "z": 2779.09, "name": "top_R"},
+            #     # {"x": 17883.77, "y" : 10166.54, "z": 2779.09, "name": "top_L"},
+            #     # {"x": 110495.44, "y": 73208.59, "z": 2776.70, "name": "bot_R"},
+            #     # {"x": 17492.82, "y": 73208.58, "z": 2776.70, "name": "bot_L"},
+            #     Position(
+            #         x=17883.77, y=10166.54, z=2779.09, name= "array", 
+            #         sequence=MDASequence(
+            #             grid_plan={"rows": 3, "columns": 4, "relative_to": "top_left", "overlap": 5, "mode": "row_wise_snake"})
+            #     ),
+            # ],
             axis_order = "pc",
         )
         return sequence
@@ -60,49 +65,70 @@ class MosaicHandler:
         return cast(str, meta.get('save_name', DEFAULT_NAME))
 
     def get_mosaic_metadata(self, sequence: MDASequence):
-        print(cast("dict", sequence.stage_positions))
-        meta = cast("dict", sequence.stage_positions.sequence)
-        print(meta)
-        print(cast(str, meta.get('stage_positions', DEFAULT_NAME)))
+        rows = int(sequence.grid_plan.rows)
+        cols = int(sequence.grid_plan.columns)
+        channels = len(sequence.channels)
+        overlap = int(sequence.grid_plan.overlap)
+        print(overlap)
+
+        idxs = np.zeros((channels, rows, cols))
+
+        # Snippet below copied from useq._iter_sequence.py
+        order = _used_axes(sequence)
+        # this needs to be tuple(...) to work for mypyc
+        axis_iterators = tuple(enumerate(_iter_axis(sequence, ax)) for ax in order)
+        for i, item in enumerate(product(*axis_iterators)):
+            if not item:  # the case with no events
+                continue  # pragma: no cover
+            # get axes objects for this event
+            index, time, position, grid, channel, z_pos = _parse_axes(zip(order, item))
+
+            idxs[index.get('c'), grid.row, grid.col, ] = i
+
+        return rows, cols, channels, overlap, idxs
+
+    def get_img(self, row, col, channel, idxs):
 
     def stitch_mosaic(self, sequence : MDASequence, img_arr):
         '''
         Assemble mosaic
         '''
         dir = self.get_dir(sequence)
-        self.get_mosaic_metadata(sequence)
+        num_rows, num_cols, num_channels, overlap, idxs = self.get_mosaic_metadata(sequence)
+        x_overlap = overlap(0)
+        y_overlap = overlap(1)
 
-        # mosaic_x_dim = (IMG_X_PX * self.num_cols) - (self.x_overlap * (self.num_cols- 1))
-        # mosaic_y_dim = (IMG_Y_PX * self.num_rows) - (self.y_overlap * (self.num_rows - 1))
+        mosaic_x_dim = (IMG_X_PX * num_cols) - (x_overlap * (num_cols- 1))
+        mosaic_y_dim = (IMG_Y_PX * num_rows) - (y_overlap * (num_rows - 1))
 
-        # mosaic = np.zeros((mosaic_y_dim, mosaic_x_dim, self.img_z_dim), dtype=np.uint32)
+        mosaic = np.zeros((num_channels, mosaic_y_dim, mosaic_x_dim), dtype=np.uint32)
 
-        # x_translation = IMG_X_PX - self.x_overlap
-        # y_translation = IMG_Y_PX - self.y_overlap
+        x_translation = IMG_X_PX - x_overlap
+        y_translation = IMG_Y_PX - y_overlap
 
-        # # Assemble mosaic
-        # print("Stitching images together")
-        # for row in tqdm(range(self.num_rows), desc="Row"):
-        #     y_start = row * y_translation
-        #     for col in tqdm(range(0, self.num_cols), desc="Column"):
-        #         x_start = col * x_translation
+        # Assemble mosaic
+        print("Stitching images together")
+        for row in tqdm(range(num_rows), desc="Row"):
+            y_start = row * y_translation
+            for col in tqdm(range(0, num_cols), desc="Column"):
+                x_start = col * x_translation
 
-        #         mosaic[y_start : y_start + IMG_Y_PX, x_start : x_start + IMG_X_PX, :] += self._get_img(row, col)
+                mosaic[y_start : y_start + IMG_Y_PX, x_start : x_start + IMG_X_PX, :] += _get_img(row, col)
 
-        # # Take average of overlapping areas
-        # print("Taking average of overlapping areas")
-        # for row in tqdm(range(1, self.num_rows), desc="Row"):
-        #     y_start = row * y_translation
-        #     mosaic[y_start : y_start - y_translation + IMG_Y_PX, :, :] = np.floor_divide(
-        #         mosaic[y_start : y_start - y_translation + IMG_Y_PX, :, :],
-        #         2
-        #     ).astype(np.uint32)
-        # for col in tqdm(range(1, self.num_cols), desc="Column"):
-        #     x_start = col * x_translation
-        #     mosaic[:, x_start : x_start - x_translation + IMG_X_PX, :] = np.floor_divide(
-        #         mosaic[:, x_start : x_start - x_translation + IMG_X_PX, :],
-        #         2
-        #     ).astype(np.uint32)
+        # Take average of overlapping areas
+        print("Taking average of overlapping areas")
+        for row in tqdm(range(1, num_rows), desc="Row"):
+            y_start = row * y_translation
+            mosaic[y_start : y_start - y_translation + IMG_Y_PX, :, :] = np.floor_divide(
+                mosaic[y_start : y_start - y_translation + IMG_Y_PX, :, :],
+                2
+            ).astype(np.uint32)
+        for col in tqdm(range(1, num_cols), desc="Column"):
+            x_start = col * x_translation
+            mosaic[:, x_start : x_start - x_translation + IMG_X_PX, :] = np.floor_divide(
+                mosaic[:, x_start : x_start - x_translation + IMG_X_PX, :],
+                2
+            ).astype(np.uint32)
 
-        # self.mosaic = mosaic.astype(self.dtype)    
+        return mosaic.astype(self.dtype)    
 
